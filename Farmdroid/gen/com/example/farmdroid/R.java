eceb2ddea5b1e0575b4ce69991514920976c138a
Pratik Lade <pratikla@buffalo.edu>
2014-10-26 12:38:26 -0400

    Added Review Screen and server call

--- Farmdroid/gen/com/example/farmdroid/R.java
+++ Farmdroid/gen/com/example/farmdroid/R.java
@@ -26,21 +26,33 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080009;
         public static final int address=0x7f080001;
+        public static final int button1=0x7f080008;
+        public static final int editText1=0x7f080003;
+        public static final int editText2=0x7f080005;
+        public static final int editText3=0x7f080007;
         public static final int marketname=0x7f080000;
+        public static final int textView1=0x7f080002;
+        public static final int textView2=0x7f080004;
+        public static final int textView3=0x7f080006;
     }
     public static final class layout {
         public static final int activity_markets=0x7f030000;
-        public static final int listview_layout=0x7f030001;
+        public static final int activity_review=0x7f030001;
+        public static final int listview_layout=0x7f030002;
     }
     public static final class menu {
         public static final int markets=0x7f070000;
+        public static final int send_json=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int rating=0x7f050004;
+        public static final int review_text=0x7f050005;
+        public static final int username=0x7f050003;
     }
     public static final class style {
         /** 
