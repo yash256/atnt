c7def4dddcd4afd68cb199be81283da8781ecf4d
Pratik Lade <pratikla@buffalo.edu>
2014-10-29 16:42:21 -0400

    Database created fresh onCreate

--- Farmdroid/src/com/example/farmdroid/MarketsDB.java
+++ Farmdroid/src/com/example/farmdroid/MarketsDB.java
@@ -15,9 +15,13 @@ public class MarketsDB {
         db.execSQL("	DROP TABLE IF EXISTS `day_of_week`;");
         db.execSQL("    CREATE TABLE IF NOT EXISTS `day_of_week` (`day_id` int(11) NOT NULL PRIMARY KEY, `day_name` varchar(20) NOT NULL);	");
         db.execSQL("	INSERT INTO `day_of_week` (`day_id`, `day_name`) VALUES (1, 'Sunday'), (2, 'Monday'), (3, 'Tuesday'), (4, 'Wednesday'), (5, 'Thursday'), (6, 'Friday'), (7, 'Saturday');	");
+        db.execSQL("	DROP TABLE IF EXISTS `location_city`;");
         db.execSQL("	CREATE TABLE IF NOT EXISTS `location_city` ( `location_id` int(11) NOT NULL PRIMARY KEY,   `name` varchar(30) NOT NULL ) ;	");
+        db.execSQL("	DROP TABLE IF EXISTS `market`;");
         db.execSQL("	CREATE TABLE IF NOT EXISTS `market` (`market_id` int(11) NOT NULL PRIMARY KEY, `Name` varchar(100) NOT NULL DEFAULT '',  `location_id` int(11) NOT NULL, `address` VARCHAR(100) NOT NULL, `from_time` VARCHAR(5) DEFAULT NULL, `to_time` VARCHAR(5) DEFAULT NULL, `from_date` int(11) DEFAULT NULL, `to_date` int(11) DEFAULT NULL, `day_of_operation` int(11) DEFAULT NULL )   ;	");
+        db.execSQL("	DROP TABLE IF EXISTS `review`;");
         db.execSQL("	CREATE TABLE IF NOT EXISTS `review` (`review_id` int(11) NOT NULL PRIMARY KEY, `market_id` int(11) NOT NULL, `user_id` int(11) NOT NULL, `rating` int(11) NOT NULL, `review_text` varchar(1024) NOT NULL, `review_date` date NOT NULL ) ;	");
+        db.execSQL("	DROP TABLE IF EXISTS `user`;");
         db.execSQL("	CREATE TABLE IF NOT EXISTS `user` ( `user_id` varchar(20) NOT NULL PRIMARY KEY, `user_name` varchar(100) NOT NULL, `user_email` varchar(100) NOT NULL );	");
         //db.execSQL("TRUNCATE TABLE `market`;");
         db.execSQL("	INSERT INTO `market` (`market_id`, `Name`, `location_id`, `address`) VALUES (1, 'Erie1', 123, '123erie'), (2, 'Erie2', 246, '246erie'), (3, 'Erie3', 369, '369erie');	 ");
